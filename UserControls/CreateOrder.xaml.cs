ffcbf7edccfcfff9387b1dc4e3ae13743919f679
VaSuOk <vasuok15vas@gmail.com>
2021-05-26 15:34:14 +0300

    add logic to createOrder

--- UserControls/CreateOrder.xaml.cs
+++ UserControls/CreateOrder.xaml.cs
@@ -22,16 +22,20 @@ namespace Сonstruction_сompany.UserControls
     /// </summary>
     public partial class CreateOrder : UserControl
     {
+        #region Data fields
         private string strName, imageName;
-        private User user;
+        private UserOrder userOrder;
         private Regions LRegions;
         private UserAge userAge;
+        #endregion
 
+        #region Constructors
         public CreateOrder(ref User user)
         {
+            userOrder = new UserOrder();
             InitializeComponent();
 
-            this.user = user;
+            this.userOrder.user = user;
             LRegions = new Regions();
             userAge = new UserAge();
 
@@ -40,7 +44,9 @@ namespace Сonstruction_сompany.UserControls
 
             InitData(user);
         }
+        #endregion
 
+        #region Methods
         public void InitData(User user)
         {
             if (user != null)
@@ -76,6 +82,38 @@ namespace Сonstruction_сompany.UserControls
                 BImg.Background = img;
             }
         }
+
+        private void SetClik(Border border)
+        {
+            border.BorderBrush = Brushes.Green;
+            border.BorderThickness = new Thickness(1, 1, 1, 1);
+            border.Background = Brushes.LightGreen;
+        }
+        private void SetUnClik(Border border1 = null, Border border2 = null, Border border3 = null, Border border4 = null, Border border5 = null)
+        {
+            if(border1 != null)
+                SetUnClik(border1);
+            if (border2 != null)
+                SetUnClik(border2);
+            if (border3 != null)
+                SetUnClik(border3);
+            if (border4 != null)
+                SetUnClik(border4);
+            if (border5 != null)
+                SetUnClik(border5);
+        }
+        private void SetUnClik(Border border)
+        {
+            border.BorderBrush = Brushes.Orange;
+            border.BorderThickness = new Thickness(2, 2, 2, 2);
+            border.Background = Brushes.Transparent;
+        }
+        #endregion
+
+        #region Events
+
+        #region Event Click
+
         private void BImage_Click(object sender, RoutedEventArgs e)
         {
             try
@@ -112,10 +150,217 @@ namespace Сonstruction_сompany.UserControls
 
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(UContent.Visibility == Visibility.Visible)
+            {
+                UContent.Visibility = Visibility.Collapsed;
+                BHiVi.Content = "▼";
+                BMain.Height = 1790;
+            }
+            else
+            {
+                UContent.Visibility = Visibility.Visible;
+                BHiVi.Content = "▲";
+                BMain.Height = 2120;
+            }
+            
+        }
         private void BImage_MouseEnter(object sender, MouseEventArgs e)
         {
             BImage.Opacity = 0.8;
         }
-        
+        #endregion
+
+        #region Event Home_Type
+        private void BprivateHome_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BprivateHome);
+            SetUnClik(BOffice, BApartment, BResting, BIndustrial);
+            userOrder.type_Building = Type_Building.Приватний;
+        }
+
+        private void BApartment_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BApartment);
+            SetUnClik(BOffice, BprivateHome, BResting, BIndustrial);
+            userOrder.type_Building = Type_Building.Квартирний;
+        }
+
+        private void BIndustrial_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BIndustrial);
+            SetUnClik(BOffice, BApartment, BResting, BprivateHome);
+            userOrder.type_Building = Type_Building.Виробничий;
+        }
+
+        private void BOffice_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BOffice);
+            SetUnClik(BprivateHome, BApartment, BResting, BIndustrial);
+            userOrder.type_Building = Type_Building.Офісний;
+        }
+
+        private void BResting_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BResting);
+            SetUnClik(BOffice, BApartment, BprivateHome, BIndustrial);
+            userOrder.type_Building = Type_Building.Оздоровчий;
+        }
+        #endregion
+
+        #region Event Type_Foundation
+        private void BF1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BF1);
+            SetUnClik(BF2, BF3, BF4);
+            userOrder.type_Foundation = Type_Foundation.Стовпчастий;
+        }
+
+        private void BF2_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BF2);
+            SetUnClik(BF1, BF3, BF4);
+            userOrder.type_Foundation = Type_Foundation.Стрічковий;
+        }
+
+        private void BF3_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BF3);
+            SetUnClik(BF2, BF1, BF4);
+            userOrder.type_Foundation = Type_Foundation.Пальовий;
+        }
+
+        private void BF4_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BF4);
+            SetUnClik(BF2, BF3, BF1);
+            userOrder.type_Foundation = Type_Foundation.Плитний;
+        }
+        #endregion
+
+        #region Event Wall_Material
+        private void BS1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BS1);
+            SetUnClik(BS2, BS3, BS4, BS5, BS6);
+            userOrder.wall_Material = Wall_Material.Селікатна_цегла;
+        }
+
+        private void BS2_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BS2);
+            SetUnClik(BS1, BS3, BS4, BS5, BS6);
+            userOrder.wall_Material = Wall_Material.Керамічна_цегла;
+        }
+
+        private void BS3_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BS3);
+            SetUnClik(BS2, BS1, BS4, BS5, BS6);
+            userOrder.wall_Material = Wall_Material.Керамічний_блок;
+        }
+
+        private void BS4_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BS4);
+            SetUnClik(BS2, BS3, BS1, BS5, BS6);
+            userOrder.wall_Material = Wall_Material.Газоблок;
+        }
+
+        private void BS5_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BS5);
+            SetUnClik(BS2, BS3, BS4, BS1, BS6);
+            userOrder.wall_Material = Wall_Material.Ракушняк;
+        }
+
+        private void BS6_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BS6);
+            SetUnClik(BS2, BS3, BS4, BS5, BS1);
+            userOrder.wall_Material = Wall_Material.Шлакоблок;
+        }
+        #endregion
+
+        #region Event Type_Roof
+        private void BD1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BD1);
+            SetUnClik(BD2, BD3, BD4, BD5);
+            userOrder.type_Roof = Type_Roof.Плаский;
+        }
+
+        private void BD2_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BD2);
+            SetUnClik(BD1, BD3, BD4, BD5);
+            userOrder.type_Roof = Type_Roof.Скатний;
+        }
+
+        private void BD3_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BD3);
+            SetUnClik(BD2, BD1, BD4, BD5);
+            userOrder.type_Roof = Type_Roof.Шатровий;
+        }
+
+        private void BD4_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BD4);
+            SetUnClik(BD2, BD3, BD1, BD5);
+            userOrder.type_Roof = Type_Roof.Двоскатний;
+        }
+
+        private void BD5_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BD5);
+            SetUnClik(BD2, BD3, BD4, BD1);
+            userOrder.type_Roof = Type_Roof.Багатоскатний;
+        }
+
+
+
+        #endregion
+
+        #region Rofing_Material
+        private void BR1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BR1);
+            SetUnClik(BR2, BR3, BR4, BR5);
+            userOrder.rofing_Material = Rofing_Material.Металочерепиця;
+        }
+
+        private void BR2_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BR2);
+            SetUnClik(BR1, BR3, BR4, BR5);
+            userOrder.rofing_Material = Rofing_Material.Металочерепиця;
+        }
+
+        private void BR3_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BR3);
+            SetUnClik(BR2, BR1, BR4, BR5);
+            userOrder.rofing_Material = Rofing_Material.Металочерепиця;
+        }
+
+        private void BR4_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BR4);
+            SetUnClik(BR2, BR3, BR1, BR5);
+            userOrder.rofing_Material = Rofing_Material.Металочерепиця;
+        }
+
+        private void BR5_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SetClik(BR5);
+            SetUnClik(BR2, BR3, BR4, BR1);
+            userOrder.rofing_Material = Rofing_Material.Металочерепиця;
+        }
+        #endregion
+
+        #endregion
+
     }
 }
---
+++ View/UserOrder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Сonstruction_сompany.View
+{
+    public enum Type_Foundation
+    {
+        Стовпчастий,
+        Стрічковий,
+        Пальовий,
+        Плитний
+    }
+    public enum Type_Building
+    {
+        Приватний,
+        Квартирний,
+        Офісний,
+        Виробничий,
+        Оздоровчий
+    }
+    public enum Type_Roof
+    {
+        Плаский,
+        Скатний,
+        Шатровий,
+        Двоскатний,
+        Багатоскатний
+    }
+    public enum Rofing_Material
+    {
+        Металочерепиця,
+        Профнастил,
+        Ондулін,
+        Шифер,
+        Фальц
+    }
+    public enum Wall_Material
+    {
+        Селікатна_цегла,
+        Керамічна_цегла,
+        Керамічний_блок,
+        Газоблок,
+        Ракушняк,
+        Шлакоблок
+    }
+    class UserOrder
+    {
+        public uint ID { get; set; }
+        public User user { get; set; }
+        public Type_Building type_Building;
+        public Type_Foundation type_Foundation { get; set; }
+        public Type_Roof type_Roof { get; set; }
+        public Wall_Material wall_Material { get; set; }
+        public Rofing_Material rofing_Material { get; set; }
+        public byte[] Project_File { get; set; }
+    }
+}
